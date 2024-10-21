2af45c60a2185d657df2cd73208821ba4abc5f34
J0rgwUNI <156668242+J0rgwUNI@users.noreply.github.com>
2024-10-21 13:21:55 +0200

    Add files via upload

---
+++ proyecto1/js/updatedTime.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var x = document.lastModified;
+    document.getElementById("demo").innerHTML = new Date(document.lastModified).toISOString().substr(11, 8);
+}
+
+myFunction()
