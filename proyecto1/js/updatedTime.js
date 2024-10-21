0f3602ddcbfc582dcea7daafaaca4d382f3483a4
J0rgwUNI <156668242+J0rgwUNI@users.noreply.github.com>
2024-10-21 13:23:17 +0200

    Delete proyecto1 directory

--- proyecto1/js/updatedTime.js
+++
@@ -1,7 +0,0 @@
-
-function myFunction() {
-    var x = document.lastModified;
-    document.getElementById("demo").innerHTML = new Date(document.lastModified).toISOString().substr(11, 8);
-}
-
-myFunction()
